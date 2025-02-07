6fe996a6679b574bd5668d84ca71e69085e8aee3
Ananya2605a <ananya0033.becse24@chitkara.edu.in>
2025-02-07 13:54:33 +0530

    Solution to Check if All Students Passed

---
+++ Check if All Students Passed@@67877d61dc107486ad5c274a/code.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+struct ad
+{
+    int r;
+    char n[20];
+    float m;
+};
+int main()
+{
+    int N,a,num;
+    scanf("%d",&N);
+    struct ad e[20];
+    for(a=0;a<N;a++)
+    {
+        scanf("%d",&e[a].r);
+        scanf("%s",e[a].n);
+        scanf("%f",&e[a].m);
+    }
+    for(a=0;a<N;a++)
+    {
+        if(e[a].m>t)
+        printf("All Passed");
+        else
+        printf("Not All Passesd")
+    }
+    return 0;
+}
