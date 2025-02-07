d62e4a3b9eb492d8c2eb6a03650dc4d2994990a5
Ananya2605a <ananya0033.becse24@chitkara.edu.in>
2025-02-07 17:27:22 +0530

    Solution to Manage Book Library

---
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+struct Book
+{
+    char t[30];
+    char a[30];
+    float p;
+};
+int main()
+{
+    int N,b,c;
+    scanf("%d",&N);
+    struct Book e[20]
+    for(b=1;b<=N;b++)
+    {
+        scanf("%s",e[b].t);
+        scanf("%s",e[b].a);
+        scanf("%f",&e[b].p);
+    }
+    scanf("%d",&c);
+    for(b=1;b<=N;b++)
+    {
+        if(e[b].p>c)
+        {
+            printg("Title: %s, Author: %s, Price: %.2f",e[b].t,e[b].a,e[b].p);
+        }
+    }
+    return 0;
+
+}
