1b0ab4ef9c848ac21bf5102f32f519efe844bbd2
Ananya2605a <ananya0033.becse24@chitkara.edu.in>
2025-02-07 18:08:31 +0530

    Solution to Toll Plaza Management System

--- Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -1,4 +1,5 @@
-/*#include <stdio.h>
+#include <stdio.h>
+#include<string.h>
 
 struct Vehicle
 {
@@ -34,39 +35,5 @@ int main()
        }
     }
     printf("Cars: %.2f, Trucks: %.2f, Bikes: %.2f",Cars,Trucks,Bikes);
-    return 0;
-}*/
-#include <stdio.h>
-#include <string.h> // Include string.h for strcmp function
-
-struct Vehicle {
-    char n[20];
-    char t[20];
-    int a;
-};
-
-int main() {
-    int a, N;
-    float Cars = 0, Trucks = 0, Bikes = 0;
-    struct Vehicle e[20];
-
-    scanf("%d", &N);
-    for (a = 0; a < N; a++) {
-        scanf("%s", e[a].n);
-        scanf("%s", e[a].t);
-        scanf("%d", &e[a].a);
-    }
-
-    for (a = 0; a < N; a++) {
-        if (strcmp(e[a].t, "Car") == 0) {
-            Cars += e[a].a;
-        } else if (strcmp(e[a].t, "Truck") == 0) {
-            Trucks += e[a].a;
-        } else {
-            Bikes += e[a].a;
-        }
-    }
-
-    printf("Cars: %.2f, Trucks: %.2f, Bikes: %.2f\n", Cars, Trucks, Bikes);
     return 0;
 }
