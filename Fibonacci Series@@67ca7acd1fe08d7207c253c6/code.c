3f228d8fa30482c85c52639382d702d4f5eeaa7c
Ananya Sharma <ananyashar06@gmail.com>
2025-03-14 20:19:30 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -3,11 +3,16 @@ int main()
 {
     int n,a=0,b,c,i;
     scanf("%d",&n);
+    if(n<=0)
+    {
+        printf("Invalid input\n");
+    }
     for(i=0;i<n;i++)
     {
         printf("%d",a);
         c=a+b;
         a=b;
         b=c;
+        return 0;
     }
 }
